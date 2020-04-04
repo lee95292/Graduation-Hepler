731ded5a980bfed8f498bf427d3c83f8a90fa7e1
lee95292 <lee95292@naver.com>
2020-04-05 06:51:46 +0900

    feat(account) signin 템플릿 추가

---
+++ frontend/src/pages/account/components/SignIn/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class SignIn extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default SignIn;
+
+SignIn.propTypes = {};
+
+SignIn.defaultProps = {};
---
+++ frontend/src/pages/account/index.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from "react";
+import Account from "./containers/Account";
+
+export class AccountPage extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Account />
+      </Fragment>
+    );
+  }
+}
+
+export default AccountPage;
